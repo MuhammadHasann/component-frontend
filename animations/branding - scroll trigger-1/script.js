8ff0b6c4a3503a56b5108f41cde54431bcdd3196
MuhammadHasann <muhammadhas772@gmail.com>
2024-01-20 18:50:53 +0700

    build(http): add component animations scroll trigger with only css

---
+++ animations/branding - scroll trigger-1/script.js
@@ -0,0 +1,23 @@
+// const branding = document.querySelector(".branding");
+// const brandingTitle = document.querySelector(".branding-title");
+// const content = document.querySelector(".content");
+
+// const heighBranding = branding.offsetHeight;
+
+// window.onscroll = () => {
+//   const result = Math.ceil((window.pageYOffset / heighBranding) * 100);
+
+//   window.pageYOffset > 0 ? (brandingTitle.style.color = "#000000") : (brandingTitle.style.color = "#8d8d8d");
+
+//   // result > 40 ? branding.classList.add("active") : branding.classList.remove("active");
+//   if (result > 40) {
+//     branding.classList.add("active");
+//     content.classList.add("active");
+//   }
+//   if (result <= 40) {
+//     branding.classList.remove("active");
+//     content.classList.remove("active");
+//   }
+
+//   document.documentElement.style.setProperty("--Z", `${result}vh`);
+// };
