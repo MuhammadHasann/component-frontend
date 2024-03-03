7e3a0bced6f1b050e8d0b621f2a3a6e7b1cb06d3
MuhammadHasann <muhammadhas772@gmail.com>
2024-03-03 14:37:21 +0700

    build(http): add section component category card

---
+++ section/category card-2/script.js
@@ -0,0 +1,10 @@
+const contents = document.querySelectorAll(".content");
+
+contents.forEach((element) => {
+  window.addEventListener("load", () => {
+    const paragraphQuote = element.querySelector(".profile_quote");
+    const height = paragraphQuote.clientHeight;
+
+    element.style.setProperty("--height_paragraph-quote", `${height}px`);
+  });
+});
